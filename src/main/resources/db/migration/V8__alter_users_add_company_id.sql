3955f28bba18ac64b9bbf4e87fffcc6c33edbb4b
Boiko_Maksym <xboikom1@stuba.sk>
2026-03-01 20:31:36 +0100

    add company_id column in users table

--- src/main/resources/db/migration/V8__alter_users_add_company_id.sql
+++ src/main/resources/db/migration/V8__alter_users_add_company_id.sql
@@ -1,8 +1,8 @@
 alter table users
-drop column if exists login,
-add column if not exists company_id int;
+drop column login,
+add column company_id int;
 
 alter table users
-    add constraint if not exists fk_users_company
+    add constraint fk_users_company
     foreign key (company_id) references companies(id)
     on delete set null;
