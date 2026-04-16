6ba6046848ab3dc8265daa6b05b5235dfdcaa7f0
Oleksii Rusanuik <oleksijrusanuk@gmail.com>
2026-04-16 23:18:07 +0200

    fix(session manager): rename method

--- src/main/java/org/example/SessionManager.java
+++ src/main/java/org/example/SessionManager.java
@@ -83,7 +83,7 @@ public final class SessionManager {
         );
     }
 
-    public synchronized Position getPosistion() {
+    public synchronized Position getPosition() {
         return currentUser.getPosition();
     }
 
